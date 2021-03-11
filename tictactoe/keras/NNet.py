20e7442ef2876e9976ba41c485e876dbee61d690
Paul Puntschart <Paul.Puntschart@cloudflight.io>
2021-03-11 20:55:44 +0100

    initial setup in jupyter notebook

--- tictactoe/keras/NNet.py
+++ tictactoe/keras/NNet.py
@@ -63,18 +63,18 @@ class NNetWrapper(NeuralNet):
         #print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
         return pi[0], v[0]
 
-    def save_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
+    def save_checkpoint(self, folder='checkpoint', filename='checkpoint.h5'):
         filepath = os.path.join(folder, filename)
         if not os.path.exists(folder):
             print("Checkpoint Directory does not exist! Making directory {}".format(folder))
             os.mkdir(folder)
         else:
-            print("Checkpoint Directory exists! ")
+            print(f"Checkpoint Directory exists (at '{os.path.abspath(filepath)}')!")
         self.nnet.model.save_weights(filepath)
 
-    def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
-        # https://github.com/pytorch/examples/blob/master/imagenet/main.py#L98
+    def load_checkpoint(self, folder='checkpoint', filename='checkpoint.h5'):
         filepath = os.path.join(folder, filename)
         if not os.path.exists(filepath):
-            raise("No model in path '{}'".format(filepath))
+            print(f"No model in path '{os.path.abspath(filepath)}'!")
+            raise(f"No model in path!")
         self.nnet.model.load_weights(filepath)
